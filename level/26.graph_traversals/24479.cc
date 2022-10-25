18511149e5caae5764b4c67325ed809b5c624b46
Jeongseop (Patrick) Yi <patrickyi0427@gmail.com>
2022-10-24 22:31:27 -0700

    24479. dfs1

---
+++ level/26.graph_traversals/24479.cc
@@ -0,0 +1,48 @@
+// acmicpc number: 24479
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int n, m, r;
+
+void dfs(int r, int& d, vector<int>& visited, vector<priority_queue<int, vector<int>, greater<int>>>& node)
+{
+    visited[r] = d;
+
+    while (!node[r].empty())
+    {
+        int tmp = node[r].top();
+        node[r].pop();
+        if (visited[tmp] == 0)
+        {
+            dfs(tmp, ++d, visited, node);
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cin >> n >> m >> r;
+
+    vector<int> visited(n+1, 0);
+    vector<priority_queue<int, vector<int>, greater<int>>> node(n+1, priority_queue<int, vector<int>, greater<int>>());
+
+    for (int i = 0; i < m; i++)
+    {
+        int s, e;
+        cin >> s >> e;
+        node[s].push(e);
+        node[e].push(s);
+    }
+
+    int d = 1;
+    dfs(r, d, visited, node);
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << visited[i] << "\n";
+    }
+}
